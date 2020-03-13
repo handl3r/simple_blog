8d1edb7cc3910430d5ed5fbce281e9b3df6a3d01
ThaiBui <thaibuixuan34@gmail.com>
2020-03-13 18:02:35 +0700

    initialize controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@
 #
 
 class User < ApplicationRecord
+  before_save { email.downcase! }
+
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: { case_sensitive: false }
   validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 20 }
@@ -20,7 +22,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :received_follows, foreign_key: :followed_user_id, class_name: 'Follow'
   has_many :followers, through: :received_follows, source: :follower
-
   has_many :given_follows, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followings, through: :given_follows, source: :followed_user
+
+  has_secure_password
 end
