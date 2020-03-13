42a23aeaa177aba32c51c61419086832669e4993
ThaiBui <thaibuixuan34@gmail.com>
2020-03-13 18:01:50 +0700

    define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
+# Something can be confuse: https://domain.com/users/1/following should be a set of users
+# these are following user with id = 1
+# and https://domain.com/users/1/followers should be a set of users that being followed by user with id = 1
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :admins
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :posts do
+    resources :comments, only: %i[create edit destroy]
+  end
+
+  resources :follows, only: %i[create destroy]
 end
