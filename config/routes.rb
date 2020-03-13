f0a437c4443ce5e50661ad7c4b421f4ca3b5b4cb
ThaiBui <thaibuixuan34@gmail.com>
2020-03-13 21:04:04 +0700

    fix coding convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-# Something can be confuse: https://domain.com/users/1/following should be a set of users
-# these are following user with id = 1
-# and https://domain.com/users/1/followers should be a set of users that being followed by user with id = 1
+# Something can be confuse: https://domain.com/users/1/following should be a set
+# of users these are following user with id = 1
+# and https://domain.com/users/1/followers should be a set of users that being
+# followed by user with id = 1
 
 Rails.application.routes.draw do
   resources :admins
