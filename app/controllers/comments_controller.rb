2ca628cbd734c6b57bda2f749f23ed3bb5ef4be1
ThaiBui <thaibuixuan34@gmail.com>
2020-03-14 17:32:48 +0700

    define some actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  # before action must login
+  def create
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
 end
--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,8 @@
 class FollowsController < ApplicationController
+  # before action must login
+  def create
+  end
+
+  def destroy
+  end
 end
