465eec7c5687fb3688ad61a81e6aef5b331ddd58
ThaiBui <thaibuixuan34@gmail.com>
2020-03-16 00:31:56 +0700

    add: load session with info in cookies before call action in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  # maybe want it to load cookies to load session before do anything in controller and then
+  before_action :current_user
 end
