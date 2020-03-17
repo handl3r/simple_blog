bf0e9feaf31c26a008f89a06a500d0a4b2655a6b
ThaiBui <thaibuixuan34@gmail.com>
2020-03-17 17:41:35 +0700

    remove before-action current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   # maybe want it to load cookies to load session before do anything in controller and then
-  before_action :current_user
+  # before_action :current_user
 end
